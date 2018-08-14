314708dc780320b4a32bf5ab64cbb69590ba9ca7
yuziyang <ziyang.yu@xiqiao.io>
2018-08-14 11:25:36 +0800

    一个springBoot的增删改查demo

--- src/main/java/com/zhiyang/demo1/GirlController.java
+++ src/main/java/com/zhiyang/demo1/GirlController.java
@@ -2,9 +2,6 @@ package com.zhiyang.demo1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
